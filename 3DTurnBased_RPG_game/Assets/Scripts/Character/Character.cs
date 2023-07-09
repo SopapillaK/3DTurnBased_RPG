51fd49e6f3bee86aca251849b2421db3d464dac6
SopapillaKat <vellita2011@gmail.com>
2023-07-09 18:01:43 -0500

    characters UI

---
+++ 3DTurnBased_RPG_game/Assets/Scripts/Character/Character.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public enum Team
+    {
+        Player,
+        Enemy
+    }
+
+    [Header("Stats")]
+    public Team team;
+    public string displayName;
+    public int curHp;
+    public int maxHp;
+
+    [Header("Components")]
+    public CharacterEffects characterEffects;
+    public CharacterUI characterUI;
+    public GameObject selectionVisual;
+    public DamageFlash damageFlash;
+
+    [Header("Prefabs")]
+    public GameObject healParticlePrefab;
+
+    private Vector3 standingPosition;    
+}
